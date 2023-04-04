43090f7bf2ffaa601e1c40a8f0913cf013dba657
sleekmason <sleekmason@gmail.com>
2023-04-04 17:55:57 -0500

    Added removal of calamares from debian installer and changed welcome script begin icon to gnome.

--- config/includes.chroot_after_packages/usr/local/bin/welcome.sh
+++ config/includes.chroot_after_packages/usr/local/bin/welcome.sh
@@ -6,7 +6,7 @@ fs=$(basename "$full_fs")
 if grep -q "$fs" /proc/partitions; then
   yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
 --width=375 --height=410 --center --escape-ok --undecorated --skip-taskbar \
---button=" Begin"!/usr/share/icons/Adwaita/22x22/legacy/preferences-desktop-font.png!:"x-terminal-emulator -T 'Customization' -e 'sudo xentry -i'" \
+--button=" Begin"!/usr/share/icons/gnome/22x22/places/debian-swirl.png!:"x-terminal-emulator -T 'Customization' -e 'sudo xentry -i'" \
 --button="gtk-ok:0" \
 --text-info --justify=left --wrap < /usr/share/lilidog/welcome.txt --fontname="Sans 11" \
 --fore="#C0DDEB"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart && exit
