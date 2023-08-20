a65d998f8bb121a0be55f895d346c4c3223fcad4
sleekmason <sleekmason@gmail.com>
2023-08-20 15:21:43 -0500

    Changed welcome.sh to welcome-backend.sh - added alias 'welcome' for new users to .bash_aliases and .zshrc

--- config/includes.chroot_after_packages/usr/local/bin/welcome-backend.sh
+++ config/includes.chroot_after_packages/usr/local/bin/welcome-backend.sh
@@ -9,12 +9,12 @@ if grep -q "$fs" /proc/partitions; then
 --button=" Begin"!/usr/share/icons/gnome/22x22/places/debian-swirl.png!:"x-terminal-emulator -T 'Customization' -e 'sudo xentry -i'" \
 --button=" Exit!application-exit:0" \
 --text-info --justify=left --wrap < /usr/share/lilidog/welcome.txt --fontname="Sans 11" \
---fore="#C0DDEB"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart && exit
+--fore="#C0DDEB"; sed -i '/welcome-backend.sh &/d' ~/.config/openbox/autostart && exit
 else
   yad --notification --image=/usr/share/icons/ld-icons/install.png --text "Install Beardog ┃ User password ▸ 'user' ┃ Root password ▸ 'live'" --command "gksudo calamares" &
   yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
 --width=375 --height=485 --center --escape-ok --undecorated --skip-taskbar \
---button=" Exit!application-exit:0" \
+--button="gtk-ok:0" \
 --text-info --justify=left --wrap < /usr/share/lilidog/welcome2.txt --fontname="Sans 11" \
---fore="#C0DDEB"; sed -i '/welcome.sh &/d' ~/.config/openbox/autostart
+--fore="#C0DDEB"; sed -i '/welcome-backend.sh &/d' ~/.config/openbox/autostart
 fi
