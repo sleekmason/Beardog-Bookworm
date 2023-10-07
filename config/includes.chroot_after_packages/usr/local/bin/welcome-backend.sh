f3459f4859ed8aab52c71dc7647e7e0c5f93d28a
sleekmason <sleekmason@gmail.com>
2023-10-07 11:21:03 -0500

    Ozark theme colors - Welcome script colors - dunst normal colors.

--- config/includes.chroot_after_packages/usr/local/bin/welcome-backend.sh
+++ config/includes.chroot_after_packages/usr/local/bin/welcome-backend.sh
@@ -10,12 +10,12 @@ xdg-mime default thunar.desktop inode/directory &&
 --button=" Begin"!/usr/share/icons/gnome/22x22/places/debian-swirl.png!:"x-terminal-emulator -T 'Customization' -e 'sudo xentry -i'" \
 --button=" Exit!application-exit:0" \
 --text-info --justify=left --wrap < /usr/share/lilidog/welcome.txt --fontname="Sans 11" \
---fore="#D8E5EB"; sed -i '/welcome-backend.sh &/d' ~/.config/openbox/autostart && exit
+--fore="#EBEBE6"; sed -i '/welcome-backend.sh &/d' ~/.config/openbox/autostart && exit
 else
   yad --notification --image=/usr/share/icons/ld-icons/install.png --text "Install Beardog ┃ User password ▸ 'user' ┃ Root password ▸ 'live'" --command "gksudo calamares" &
   yad --title "Welcome!" --window-icon=/usr/share/icons/ld-icons/paw-color.png \
 --width=375 --height=485 --center --escape-ok --undecorated --skip-taskbar \
 --button="gtk-ok:0" \
 --text-info --justify=left --wrap < /usr/share/lilidog/welcome2.txt --fontname="Sans 11" \
---fore="#D8E5EB"; sed -i '/welcome-backend.sh &/d' ~/.config/openbox/autostart
+--fore="#EBEBE6"; sed -i '/welcome-backend.sh &/d' ~/.config/openbox/autostart
 fi
